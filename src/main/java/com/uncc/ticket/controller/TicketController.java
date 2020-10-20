171df5a731d273a71b30cdc73841f4450145c7b2
[wpham7] <wesleypham7.wp@gmail.com>
2020-10-20 19:19:36 -0400

    New updated code as of Lab 3

--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -53,12 +53,15 @@ public class TicketController {
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
         // Code here
-        return "redirect:/"; //Remove this line
+        TicketEntity a = ticketService.findById(id);
+        model.addAttribute("ticket", a);
+        return "tickets/storeTicket";
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
     public String deleteTicket(@PathVariable("id") Long id) {
         // Code here
+        ticketService.deleteById(id);
         return "redirect:/";
     }
 
